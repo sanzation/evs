8ad934115567c736125c9c775ade59880875480e
sanzation <dbaehrle@yahoo.de>
2018-01-01 23:59:00 +0100

    hoho

--- src/pages/opm/opm.ts
+++ src/pages/opm/opm.ts
@@ -12,42 +12,136 @@ import { Chart } from 'chart.js';
  * Ionic pages and navigation.
  */
 
+export interface IcatData{
+		perfDiff: any;
+		perfDiffCol: string;
+		select: string;
+	};
+export class catData implements IcatData{
+		perfDiff: any;
+		perfDiffCol: string;
+		select: string;
+}
+
+
+
 @IonicPage()
 @Component({
   selector: 'page-opm',
   templateUrl: 'opm.html'
 })
 export class OpmPage {
+	//graph
 	@ViewChild('lineCanvas') lineCanvas;
 	lineChart:any;
+	//data
 	EvsData:any;	
 	comPerf:any;
 	depPerf:any;
-	comPerfDiff: any;
 	
-	comPerfDiffColor: string;
-	comPerfSelect: string;
-	depPerfSelect: string;
+	
+
+	comCat: catData;
+
+	depCat: catData;
+	comfCat: catData;
+	depfCat: catData;
+	openCat: catData;
+	opennCat: catData;
+	stTryCat: catData;
+	rsTryCat: catData;
+	stHbwCat: catData;
+	saleCat: catData;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private EvsCall : EvscallProvider) {
-
+	  this.comCat= new catData();
+	  this.depCat= new catData();
+	  this.openCat= new catData();
   }
   ionViewDidLoad() {
-	var gDate=new Date();
-	this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+
+   this.EvsCall.getData().subscribe(EvsData=>{
+	this.EvsData= EvsData.current_observation;
+	console.log(EvsData);
+	   this.comPerf=EvsData.rows[0].elements[0].distance.text;
+	   this.depPerf=EvsData.rows[0].elements[0].distance.value;
+
+     });
+
+
+  }
+ionViewWillEnter()
+	{
+
+	this.comCat.perfDiff=Math.random() * (1 + 2) - 2;
+	this.comCat.perfDiffCol= this.comCat.perfDiff>0 ?  'danger' : 'secondary';
+
+
+	}
+	
+const actGraph= (area : string): void =>{
+	
+	this.comCat.select = area =='com' ? '#f0f0f0' : '#ffffff';
+	this.depCat.select = area =='dep' ? '#f0f0f0' : '#ffffff';
+	this.openCat.select = area =='open' ? '#f0f0f0' : '#ffffff';
+
+	let drawGraph= ( area : string): void => {
+	
+		const perfdatafunc= (area: string) : any => {
+			let data= {
+			'com': () => {return  [1234,8700,1233,7999];},
+			'dep': () => {return  [5674,122,4505,423];},
+			'open': () => {return  [96964,43434,59544,13490];},
+			'default': () => {return  [0,0,0,0];}
+			}; 
+		
+			return (data[area]||data['default'])();	
+		}
+		const colorget= (area: string) : string => {
+			let data={
+				'primary':    () => {return'#16292C'},
+				'secondary':  () => {return'#259ee7'},
+				'danger':     () => {return'#db7712'},
+				'light':      () => {return'#d8d8d8'},
+				'white':	() => {return '#ffffff'},
+				'dark':       () => {return '#545454'},
+				'com':		() => {return'#002f7c'},
+				'dep':		() => {return'#00707c'},
+				'slip':		() => {return'#00067c'},
+				'opm':		() => {return'#99b3dd'},
+				'dps':		() => {return'#d17373'},
+				'cps':		() => {return'#9bbb59'},
+				'dpick':	() => {return'#7C0000'},
+				'drpck':	() => {return'#af3b3b'},
+				'cpsa':		() => {return'#007c18'},
+				'cpsd':		() => {return'#467c00'},
+				'we':		() => {return'#c164c1'},
+				'wa':		() => {return'#ccbd7c'},
+			 	'inlay':	() => {return'#C9AE91'},
+				'kk':		() => {return'#029DDD'},
+				'misc':		() => {return'#93a2ba'},
+				'default':	() => {return'#ffffff'}
+			};
+			return (data[area]||data['default'])();
+		}	
+	        var perfdata = perfdatafunc(area);
+		var labeldata  = 		
+			["22:00","23:00","00:00","01:00"];			
+		
+		this.lineChart = new Chart(this.lineCanvas.nativeElement, {
 	 
 		    type: 'line',
 		    data: {
-			labels: [
-			"22:00","23:00","00:00","01:00"			
-				],
+			labels: labeldata
+				,
 			datasets: [
 			    {
-				label: "OPM",
+				label: area,
 				fill: false,
 				lineTension: 0.1,
-				backgroundColor: "rgba(75,192,192,0.4)",
+				backgroundColor: colorget(area),
 				borderColor: "rgba(75,192,192,1)",
 				borderCapStyle: 'butt',
 				borderDash: [],
@@ -62,40 +156,31 @@ export class OpmPage {
 				pointHoverBorderWidth: 2,
 				pointRadius: 1,
 				pointHitRadius: 10,
-				data: [6500, 5904,3123,5105],
+				data: perfdata,
 				spanGaps: false,
 			    }
-			]
-		    }
-	 
+			],
+			    
+		    },
+		 options: {
+                    maintainAspectRatio: false,
+			legend: {
+			    display: true,
+			    position: 'top',
+				    labels: {
+					fontColor: 'rgb(0, 0, 0)'
+					    }
+				}
+                }
 		});
 
-   this.EvsCall.getData().subscribe(EvsData=>{
-	this.EvsData= EvsData.current_observation;
-	console.log(EvsData);
-	   this.comPerf=EvsData.rows[0].elements[0].distance.text;
-	   this.depPerf=EvsData.rows[0].elements[0].distance.value;
-
-     });
-
-  }
-ionViewWillEnter()
-	{
- console.log('ionViewWillEnter OpmPage');
-
-	this.comPerfDiff=Math.random() * (1 + 2) - 2;
-	this.comPerfDiffColor= this.comPerfDiff>0 ?  'danger' : 'secondary';
 
 
-	}
-actGraph(area : string){
-	console.log('actGraph ');
+		}
 
-	
-	this.comPerfSelect = area =='COM' ? '#f0f0f0' : '#ffffff';
-	this.depPerfSelect = area =='DEP' ? '#f0f0f0' : '#ffffff';
-	console.log(area);
+	drawGraph(area);
 
-}	
+}
+
 
 }
