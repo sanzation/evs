4c801fad4534001956bab5e3d300e2e66953d1fe
Dario Bährle <DBaehrle@sbs07wmsdom.local>
2018-03-21 13:46:04 +0100

    merge

---
+++ src/providers/evscall/evscall.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the EvscallProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+
+
+@Injectable()
+export class EvscallProvider {
+
+	
+	//test : string="http://10.29.32.68:8100/api";
+	test : string="http://evs.sbs07wmsdom.local:38317/EvsDataServiceWeb"
+	ent : string="/getDataEnt";
+	list : string="/getDataList";
+	rou : string="/getRouList";
+	fill : string="/getFillList";
+	cam : string="/getCamList";
+
+  constructor(public http: Http) {
+  }
+	getData(){
+	return this.http.get(`${this.test}${this.ent}`)
+		.map(res => res.json());
+	
+	}
+	
+	getList(){
+	return this.http.get(`${this.test}${this.list}`)
+		.map(res => res.json());
+	
+	}
+	
+	getRou(){
+	return this.http.get(`${this.test}${this.rou}`)
+		.map(res => res.json());
+	
+	}
+	getCam(){
+	return this.http.get(`${this.test}${this.cam}`)
+		.map(res => res.json());
+	
+	}
+	getFill(){
+	return this.http.get(`${this.test}${this.fill}`)
+		.map(res => res.json());
+	
+	}
+}
+
